23eea3cf39b2e2e26e8608f70e57ee186e2fee40
vbood <viktorfbood@gmail.com>
2025-09-24 15:42:11 +0200

    early test esp32 demo

--- sql/temperature_db.sql
+++ sql/temperature_db.sql
@@ -1,10 +1,10 @@
-drop database temperatureDB;
-create database temperatureDB;
-use temperatureDB;
+drop database if exists testtemp;
+create database testtemp;
+use testtemp;
 
 create table temperature (
-    temperature_id int not null auto_increment primary key,
-    temperature_value int not null,
+    id int not null auto_increment primary key,
+    value double not null,
     created_at datetime default current_timestamp,
     update_at datetime default current_timestamp on update current_timestamp
 );
--- src/main/java/com/example/hibernatedemo/TemperatureDemoApplication.java
+++ src/main/java/com/example/hibernatedemo/TemperatureDemoApplication.java
@@ -2,10 +2,19 @@ package com.example.hibernatedemo;
 
 import com.example.hibernatedemo.models.Temperature;
 import com.example.hibernatedemo.repos.TemperatureRepository;
+import com.fazecast.jSerialComm.SerialPort;
+
+import java.io.BufferedReader;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import com.fazecast.jSerialComm.SerialPort;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 
 @SpringBootApplication
 public class TemperatureDemoApplication {
@@ -17,10 +26,50 @@ public class TemperatureDemoApplication {
     @Bean
     public CommandLineRunner demoTemperature(TemperatureRepository tempRepo) {
         return (args) -> {
-            // Creates temperature values for the temperature table
-            tempRepo.save(new Temperature(22.5));
-            tempRepo.save(new Temperature(23.0));
-            tempRepo.save(new Temperature(21.8));
+            final String PORT_NAME = "COM3";
+            final int BAUD = 115200;
+
+            SerialPort port = SerialPort.getCommPort(PORT_NAME);
+            port.setBaudRate(BAUD);
+            port.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
+
+
+            if (!port.openPort()){
+                System.err.println("Fel inträffade kunde inte öppnar " + PORT_NAME);
+                return;
+            }
+            System.out.println("Öppnat och lyssnar på " + PORT_NAME);
+            // logiken och för att testa läsa esp32 serial och spara värde till db
+            Thread t = new Thread(() -> {
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(port.getInputStream(), StandardCharsets.UTF_8))){
+                    String line;
+                    while ((line = br.readLine()) != null){
+                        line = line.trim();
+                        if (line.isEmpty()) continue;
+                        try {
+                            double val = Double.parseDouble(line);
+                            tempRepo.save(new Temperature(val));
+                            System.out.println("Sparat " + val + " C");
+                        }
+                        catch (Exception ex) {
+                            System.err.println("Felser");
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                finally{
+                    try {
+                    port.closePort();
+                     } 
+                    catch (Exception ignore){}
+                        
+                }
+
+            }, "Thread-lyssnare");
+            t.setDaemon(true);
+            t.start();
         };
     }
 }
